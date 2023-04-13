29b17b663615cfafad9ce983aae24591c4ee9dc5
mizutamasato <mizuta1masato@gmail.com>
2023-04-13 23:45:44 +0900

    4章完了

--- src/Sample.java
+++ src/Sample.java
@@ -1,6 +1,18 @@
-
+/*サンプルプログラム
+  開発者　ミスター　作成日4/12　*/
 public class Sample {	 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+// ここからメインメソッド
+	public static void main(String[] args) {
+		System.out.println("ようこそ占いの館へ");
+		System.out.println("あなたの名前を教えてください");
+		String name = new java.util.Scanner(System.in).nextLine();
+		System.out.println("あなたの年齢を入力してください");
+		String ageString = new java.util.Scanner(System.in).nextLine();
+		int age = Integer.parseInt(ageString);
+		int fortune = new java.util.Random().nextInt(4);
+		fortune++ ;
+		System.out.println("占いの結果がでました！！");
+		System.out.println(age + "歳の" + name + "さん、あなたの運気番号は" + fortune + "です！");
+		System.out.println("1;大吉 2:中吉 3:吉 4:末吉");
     }
 }
