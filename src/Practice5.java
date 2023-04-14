bf1da62b124751c891f3cd96ab0cc4e0aa7a986d
mizutamasato <mizuta1masato@gmail.com>
2023-04-15 01:47:47 +0900

    5章まで完了

---
+++ src/Practice5.java
@@ -0,0 +1,19 @@
+//作成日４／１４
+public class Practice5 {
+	public static void main(String[] args) {
+		introduceOneself();
+	}
+	
+	public static void introduceOneself() {
+		String name = "ミスター";
+		int age = 28;
+		double hieght = 170.0;
+		char eto = '宇';
+		System.out.println("我は" + name);
+		System.out.println(age + "歳の");
+		System.out.println(hieght + "センチで干支は");
+		System.out.println( eto +"だぁ");
+	}
+	
+
+}
